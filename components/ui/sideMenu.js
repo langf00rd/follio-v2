6db86a94e0b02ab048912dcfd263e0e528dc3511
Langford Quarshie <langfordquarshie21@gmail.com>
2022-04-28 13:12:15 +0000

    side menu active nav item highllight

--- components/ui/sideMenu.js
+++ components/ui/sideMenu.js
@@ -1,33 +1,41 @@
-import Link from "next/link"
 import { headerStyles } from "../styles/headerStyles"
+import Link from "next/link"
+import { useEffect, useState } from "react"
 import Button from "./buttons/button"
 import Nav from "./header/nav"
 import PageControlLinks from "./sidebar/pageLinks"
 
 const styles = {
     menuLink: `opacity-80 mb-5 cursor-pointer hover:opacity-100 hover:text-brand user-select-none`,
+    active: `opacity-80 mb-5 cursor-pointer hover:opacity-100 text-brand user-select-none`,
     sideMenu: `p-5 side-menu bg-white fixed border-b border-b-mid w-screen top-18 left-0 z-50`,
 }
 
 const SideMenu = () => {
+    const [route, setRoute] = useState("/")
+
+    useEffect(() => {
+        setRoute(window.location.pathname)
+    }, [route])
+
     return <div className={styles.sideMenu}>
         <Link passHref={true} href="/account/analytics">
-            <p className={styles.menuLink}>
+            <p className={route === "/account/analytics" ? styles.active : styles.menuLink}>
                 Analytics
             </p>
         </Link>
         <Link passHref={true} href="/account/edit">
-            <p className={styles.menuLink}>
+            <p className={route === "/account/edit" ? styles.active : styles.menuLink}>
                 Edit
             </p>
         </Link>
         <Link passHref={true} href="/account/themes">
-            <p className={styles.menuLink}>
+            <p className={route === "/account/themes" ? styles.active : styles.menuLink}>
                 Themes
             </p>
         </Link>
         <Link passHref={true} href="/account/settings">
-            <p className={styles.menuLink}>
+            <p className={route === "/account/settings" ? styles.active : styles.menuLink}>
                 Settings
             </p>
         </Link>
