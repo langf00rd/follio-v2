89c8e1d29d3c9d965c3cf28f6b06225e591ea44b
Langford Kwabena <55645613+langfordquarshie21@users.noreply.github.com>
2022-05-23 19:02:39 +0000

    ...

--- pages/[usernames].js
+++ pages/[usernames].js
@@ -45,20 +45,20 @@ const Page = () => {
 
     if (loading) return <Loader />
 
-    // if ((data && data) && data.theme == 1) return <Theme1 editMode={false} data={data} />
-    // if ((data && data) && data.theme == 2) return <Theme2 editMode={false} data={data} />
-    // if ((data && data) && data.theme == 3) return <Theme3 editMode={false} data={data} />
+    if ((data && data) && data.theme == 1) return <Theme1 editMode={false} data={data} />
+    if ((data && data) && data.theme == 2) return <Theme2 editMode={false} data={data} />
+    if ((data && data) && data.theme == 3) return <Theme3 editMode={false} data={data} />
 
-    return <Theme1 editMode={false} data={data} />
+    // return <Theme1 editMode={false} data={data} />
 
-    // if (!exists) return <div className="w-screen h-screen flex flex-col items-center justify-center">
-    //     <p className="sm:text-3xl text-2xl mb-10">Oops! site has no owner {data}</p>
-    //     <a href="https://follio.app">
-    //         <Button label="Claim this domain" />
-    //     </a>
-    // </div>
+    if (!exists) return <div className="w-screen h-screen flex flex-col items-center justify-center">
+        <p className="sm:text-3xl text-2xl mb-10">Oops! site has no owner {data}</p>
+        <a href="https://follio.app">
+            <Button label="Claim this domain" />
+        </a>
+    </div>
 
-    // return null
+    return null
 }
 
 export default Page
